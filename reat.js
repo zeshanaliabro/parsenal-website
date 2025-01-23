132009d75f529848950be43f9a2daa1d68461c98
zeshan ali <zehsan290@gmail.com>
2025-01-23 12:28:52 -0700

    first commit

---
+++ reat.js
@@ -0,0 +1,27 @@
+function send(e){
+    // e.preventDefault()
+    let pursom = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value,
+    }
+    console.log(pursom)
+    const serviceID = 'service_60lkorh'
+    const templateID = 'template_pgvoaz9'
+emailjs.send(serviceID,templateID,pursom).then(
+    (response) =>{
+document.getElementById('name').value="";
+document.getElementById('email').value="";
+document.getElementById('message').value="";
+
+console.log('right', response );
+alert("your messege is success")
+    },
+    (error) => {
+        console.log("feilde..." , error)
+        alert("your message is feilde")
+    },
+);
+
+}
+
