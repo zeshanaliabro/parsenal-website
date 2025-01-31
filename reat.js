5f57997e9312744e297f65b15b96b5ce57b5d2af
zeshan ali <zehsan290@gmail.com>
2025-01-31 10:44:57 -0700

    changes

--- reat.js
+++ reat.js
@@ -1,11 +1,13 @@
-function send(){
+
+function send(event){
+    event.preventDefault(event);
     let pursom = {
         name: document.getElementById('name').value,
         email: document.getElementById('email').value,
         message: document.getElementById('message').value,
     }
     console.log(pursom)
-    const serviceID = 'service_60lkorh'
+    const serviceID = 'service_4nh7qxg'
     const templateID = 'template_pgvoaz9'
 emailjs.send(serviceID,templateID,pursom).then(
     (response) =>{
